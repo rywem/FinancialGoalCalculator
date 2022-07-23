ea875e42cf992c40e8ce7997c0563e28330064ed
Ryan <cybervoid41@gmail.com>
2022-07-23 09:12:29 -0600

    Add Migration Owner Column

---
+++ FinancialGoalCalculator.Web/Migrations/20220723151215_AddOwnerColumnToAccount.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancialGoalCalculator.Web.Migrations
+{
+    public partial class AddOwnerColumnToAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Owner",
+                table: "Account",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Owner",
+                table: "Account");
+        }
+    }
+}
